e8fd6f06490a20e610b971476bed68030c770d73
Philip Boen <philipboen001@gmail.com>
2023-05-04 13:12:13 +0300

    First commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import './Main.css'
+
+function Main() {
+  return (
+    <div className="Main">
+        <h1>Happy Coding</h1>
+    </div>
+  )
+}
+
+export default Main
