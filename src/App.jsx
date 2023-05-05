2290f803b969b60af90b059d69ee0e4de9dc82b0
Philip Boen <philipboen001@gmail.com>
2023-05-05 10:39:51 +0300

    Uploaded new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 
-import Main from './components/Main'
+import Content from './components/Content'
 
 function App() {
 
   return (
     <div className='App'>
-      <Main />
+      <Content />
     </div>
   )
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import './Content.css'
+
+function Content() {
+  return (
+    <div className="Content">
+        <h1>Happy Coding</h1>
+    </div>
+  )
+}
+
+export default Content
