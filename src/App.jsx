0a3f3f773efe35fb56652b5dfca8c845f3216a83
Philip Boen <philipboen001@gmail.com>
2023-10-05 16:20:15 +0300

    added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 
-import Content from './components/Content'
-
 function App() {
 
   return (
     <div className='App'>
-      <Content />
+      <p>Happy Coding</p>
     </div>
   )
 }
